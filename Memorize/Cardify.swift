639739de3e195031982a3e94acf419193c3913b7
James Spece <specej@gmail.com>
2020-10-26 19:32:00 -0400

    End of Lecture 6 state

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -7,20 +7,35 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    // rename "animatableData" with computed properties
+    var animatableData: Double {
+        get { return rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         ZStack {
-            // If face up, show the content of the card and a border
-            if isFaceUp {
+            //Front of the card, shown only when face up
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            //Back of the card, shown only when not face up
+            RoundedRectangle(cornerRadius: cornerRadius).fill().opacity(isFaceUp ? 0 : 1)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0,1,0))
     }
     private let cornerRadius: CGFloat = 10.0
     private let edgeLineWidth: CGFloat = 3.0
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -20,13 +20,17 @@ struct EmojiMemoryGameView: View {
                 Text(viewModel.themeName)
                 Spacer()
                 Button("New Game") {
-                    viewModel.newGame()
+                    withAnimation(.easeInOut) {
+                        viewModel.newGame()
+                    }
                 }
             }
             .padding(.horizontal)
             Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    viewModel.choose(card: card)
+                    withAnimation(.linear) {
+                        viewModel.choose(card: card)
+                    }
                 }
                 .padding(5)
             }
@@ -40,14 +44,36 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     var card: MemoryGame<String>.Card
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             if card.isFaceUp || !card.isMatched {
                 ZStack {
-                    PieView(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
+                    Group {
+                        if card.isConsumingBonusTime {
+                            PieView(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                                .onAppear {
+                                    self.startBonusTimeAnimation()
+                                }
+                        } else {
+                            PieView(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                        }
+                    }.padding(5).opacity(0.4)
                     Text(card.content)
+                        // implicit animation from lecture 6.
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                        .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
+                        .font(Font.system(size: fontSize(for: geometry.size)))
                 }.cardify(isFaceUp: card.isFaceUp)
-                .font(Font.system(size: fontSize(for: geometry.size)))
+                .transition(AnyTransition.scale)
             }
         }
         // Lecture 2 homework item #3 - no longer needed with Grid
